a929d0d8caf3d3f9c9eb357adfe842dce81a9365
Philipp Kalytta (Endlesscrafter) <git@kalytta.net>
2018-08-17 17:36:15 +0200

    Changed Logging to PAX

--- src/main/java/net/kalytta/osgi/webserver/WebserverActivator.java
+++ src/main/java/net/kalytta/osgi/webserver/WebserverActivator.java
@@ -2,11 +2,26 @@ package net.kalytta.osgi.webserver;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import net.kalytta.osgi.logging.LoggingService;
+import org.osgi.framework.ServiceReference;
 
 public class WebserverActivator implements BundleActivator {
 
+    private LoggingService loggingService;
+
     public void start(BundleContext bundleContext) throws Exception {
-        System.out.println("Webserver Bundle Started");
+        //LoggingService heranholen
+        ServiceReference[] refs = bundleContext.getServiceReferences("net.kalytta.osgi.logging.LoggingService", null);
+        if (refs != null) {
+            System.out.println(refs[0].getBundle());
+            loggingService = (LoggingService) bundleContext.getService(refs[0]);
+        }
+        if(loggingService == null){
+            System.out.println("[Core] FATAL No Logging Service found!");
+            throw new IllegalStateException();
+        }
+
+        loggingService.info("Webserver","module started.");
     }
 
     public void stop(BundleContext bundleContext) throws Exception {
